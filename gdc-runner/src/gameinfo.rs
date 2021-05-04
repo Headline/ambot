42016682079df927b16c701b6286afce616067ca
Michael Flaherty <michaelwflaherty@me.com>
2021-05-04 00:37:02 -0700

    Add proper engine name parameter

--- gdc-runner/src/gameinfo.rs
+++ gdc-runner/src/gameinfo.rs
@@ -3,6 +3,7 @@ pub struct Game {
     pub appid : i32,
     pub name : &'static str,
     pub gamedir : &'static str,
+    pub engine : &'static str,
 }
 
 pub struct GameCache {
@@ -12,10 +13,10 @@ impl GameCache {
     pub fn new() -> GameCache {
         GameCache {
             cache: vec! [
-                Game { appid: 740, name: "csgo", gamedir: "csgo" },
-                Game { appid: 232250, name: "tf2", gamedir: "tf" },
-                Game { appid: 232330, name: "css", gamedir: "cstrike" },
-                Game { appid: 222860, name: "l4d2", gamedir: "left4dead2" },
+                Game { appid: 740, name: "csgo", gamedir: "csgo", engine: "csgo" },
+                Game { appid: 232250, name: "tf2", gamedir: "tf", engine: "orangebox_valve" },
+                Game { appid: 232330, name: "css", gamedir: "cstrike", engine: "css" },
+                Game { appid: 222860, name: "l4d2", gamedir: "left4dead2", engine: "left4dead2" },
             ]
         }
     }
